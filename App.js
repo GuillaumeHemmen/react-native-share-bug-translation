ae682e7e34044b60135da09ca87bb0acc4f40a85
Guillaume "B.B." Van Hemmen <GuillaumeHemmen@users.noreply.github.com>
2020-11-30 12:31:18 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,102 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    TouchableOpacity,
+} from 'react-native';
+
+import {
+    Header,
+    Colors,
+} from 'react-native/Libraries/NewAppScreen';
+import Share from 'react-native-share';
+
+const App: () => React$Node = () => {
+    const stickerPDF = {
+        mimeType: 'application/pdf',
+        content: 'aW1hZ2UgZXhhbXBsZQ==',
+    };
+
+    return (
+        <>
+            <StatusBar barStyle="dark-content"/>
+            <SafeAreaView>
+                <ScrollView
+                    contentInsetAdjustmentBehavior="automatic"
+                    style={styles.scrollView}>
+                    <Header/>
+                    {global.HermesInternal == null ? null : (
+                        <View style={styles.engine}>
+                            <Text style={styles.footer}>Engine: Hermes</Text>
+                        </View>
+                    )}
+                    <View style={styles.body}>
+                        <View style={styles.sectionContainer}>
+                            <Text style={styles.sectionTitle}>Share.open PDF</Text>
+                            <TouchableOpacity onPress={() => {
+                                const shareOptions = {
+                                    type: 'application/pdf',
+                                    url: `data:${stickerPDF.mimeType};base64,${stickerPDF.content}`,
+                                };
+                                Share.open(shareOptions).catch(/* istanbul ignore next */ () => null);
+                            }} style={{backgroundColor: 'grey'}}><Text>Share.open PDF button</Text></TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    scrollView: {
+        backgroundColor: Colors.lighter,
+    },
+    engine: {
+        position: 'absolute',
+        right: 0,
+    },
+    body: {
+        backgroundColor: Colors.white,
+    },
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+        color: Colors.black,
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+        color: Colors.dark,
+    },
+    highlight: {
+        fontWeight: '700',
+    },
+    footer: {
+        color: Colors.dark,
+        fontSize: 12,
+        fontWeight: '600',
+        padding: 4,
+        paddingRight: 12,
+        textAlign: 'right',
+    },
+});
+
+export default App;
